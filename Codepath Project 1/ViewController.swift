8b7ca8f8020c4cb93178e755d33cbed717074b5c
shuvangee <144741662+shuvangee@users.noreply.github.com>
2025-01-31 21:52:10 -0600

    Initial Commit

---
+++ Codepath Project 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Codepath Project 1
+//
+//  Created by Shuvangee Seth on 1/31/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Codepath Project 1Tests/Codepath_Project_1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Codepath_Project_1Tests.swift
+//  Codepath Project 1Tests
+//
+//  Created by Shuvangee Seth on 1/31/25.
+//
+
+import Testing
+@testable import Codepath_Project_1
+
+struct Codepath_Project_1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
